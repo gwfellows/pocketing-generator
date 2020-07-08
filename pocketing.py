eaffae5e00cc4cbb84506c1703a0a61d3349546e
grantfellows <56371699+grantfellows@users.noreply.github.com>
2020-07-07 17:29:34 -0700

    add support for arcs in outer polygon

--- pocketing.py
+++ pocketing.py
@@ -3,6 +3,10 @@ from solid import *
 from solid.utils import *
 import os
 import sys
+import math
+
+#$fn
+SEGMENTS = 48 
 
 # python pocket .py <input_file> <thickness> <radius> <output_file>
 inputPath = sys.argv[1]
@@ -11,7 +15,6 @@ radius = float(sys.argv[3])
 outputPath = sys.argv[4]
 
 dxf = ezdxf.readfile(inputPath)
-
 mdl = dxf.modelspace()
 
 #get hole definitions from circles
@@ -32,20 +35,36 @@ for p in polylines:
         strutDefs.append([p.__getitem__(n).dxf.location, p.__getitem__(n+1).dxf.location])
         n += 1
 
-#get outer plate shape from non-circle non-construction shape
+def arcPoly(center, a1, a2, r):
+    points = []
+    t = 0
+    while t <+ 1:
+        a = a2-a1
+        # assume smaller angle, in the future I should fix this
+        if a <= 0:
+            a += math.pi*2
+        points.append([center[0] + r*math.cos(a1 + a*t), center[1] + r*math.sin(a1 + a*t)])
+        t += 0.1
+    return points
+
+#get outer plate shape from non-construction polyline
 outerPoly = []
 outer = mdl.query('POLYLINE[!linetype=="DASHED"]')
 n = 0
 for o in outer: 
      n = 0
      while n <= o.__len__()-1:
-        outerPoly.append(o.__getitem__(n).dxf.location)
-        print(o.__getitem__(n).dxf.bulge)
+        bulge = o.__getitem__(n).dxf.bulge
+        pos = o.__getitem__(n).dxf.location
+        if o.__getitem__(n).dxf.bulge == 0:
+            outerPoly.append([pos[0], pos[1]])
+        else:
+            posNext = (o.__getitem__((n+1) %  o.__len__())).dxf.location
+            for p in arcPoly(*ezdxf.math.bulge_to_arc(pos,posNext,bulge)):
+                outerPoly.append(p)
         n += 1
 
-
-#$fn
-SEGMENTS = 48 
+print(outerPoly)
 
 #generate pocketed geometry
 #thickness = min thickness of any part
@@ -82,19 +101,13 @@ def pocketedPlate(thickness, radius):
             )
         return union()(struts)
 
-    
     a = fillet(((polygon(outerPoly) - offset(delta = -thickness)(polygon(outerPoly)) + addStruts() + addHoles(thickness)) * polygon(outerPoly)), radius) - addHoles(0)
 
     return a
 
-
 a = pocketedPlate(thickness, radius)
-
 scad_render_to_file(a, file_header=f'$fn = {SEGMENTS};', include_orig_code=True)
-
-os.system('cmd /c "openscad.exe -o "' + outputPath + '" pocketing.scad"')
-
+os.system('cmd /c ""C:\Program Files\OpenSCAD\openscad.com" -o "' + outputPath + '" pocketing.scad"')
 
 # TODO:
-# - add support for arcs in outer poly
 # - switch to freecad scripting? (for true arcs)
